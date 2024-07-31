64c3cf3d64d0654a63d98772e19a687838bb019a
AiminoFrancisco <franciscoaimino2001@gmail.com>
2024-07-31 17:33:03 -0300

    styles

--- src/components/Contact/Map.js
+++ src/components/Contact/Map.js
@@ -9,7 +9,7 @@ const Map = () => (
     <div style={{ marginBottom: '30px', marginTop: '20px'}}>
       <Opinion />
     </div>
-    <div style={{height:'50%'}}>
+    <div style={{height:'50%', paddingBottom:'30px'}}>
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3406.455608076221!2d-64.23154292512015!3d-31.373998594202202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943299525b9b6855%3A0x9d795ce46699ca3d!2sHEALTH%20%26%20TRAINING%20GYM!5e0!3m2!1ses-419!2sar!4v1721923035469!5m2!1ses-419!2sar"
       width="600"
@@ -25,7 +25,7 @@ const Map = () => (
 );
 
 const styles = css`
-  width: 60%;
+  width: 80%;
   max-width: 60%;
   min-height: 70vh;
   margin: 0 auto; /* Centra el contenedor horizontalmente */
